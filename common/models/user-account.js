310fc864af696595b1e27bd124be5a7145f8fcce
Felipe Antero <souzantero@gmail.com>
2018-10-12 14:11:14 -0300

    disable relation endpoint of user account model file

--- common/models/user-account.js
+++ common/models/user-account.js
@@ -1,5 +1,21 @@
 'use strict';
 
 module.exports = function(Model) {
+  // SETTINGS
 
+  Model.disableRemoteMethodByName('prototype.__count__accessTokens');
+  Model.disableRemoteMethodByName('prototype.__create__accessTokens');
+  Model.disableRemoteMethodByName('prototype.__delete__accessTokens');
+  Model.disableRemoteMethodByName('prototype.__destroyById__accessTokens');
+  Model.disableRemoteMethodByName('prototype.__findById__accessTokens');
+  Model.disableRemoteMethodByName('prototype.__get__accessTokens');
+  Model.disableRemoteMethodByName('prototype.__updateById__accessTokens');
+
+  Model.disableRemoteMethodByName('prototype.__count__identities');
+  Model.disableRemoteMethodByName('prototype.__create__identities');
+  Model.disableRemoteMethodByName('prototype.__delete__identities');
+  Model.disableRemoteMethodByName('prototype.__destroyById__identities');
+  Model.disableRemoteMethodByName('prototype.__findById__identities');
+  Model.disableRemoteMethodByName('prototype.__get__identities');
+  Model.disableRemoteMethodByName('prototype.__updateById__identities');
 };
