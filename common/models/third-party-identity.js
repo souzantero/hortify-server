48e72fbfd7179395d0ac05c9fb776d3c2aba10ca
Felipe Antero <souzantero@gmail.com>
2018-10-12 19:20:06 -0300

    add reset password and confirm email feature

--- common/models/third-party-identity.js
+++ common/models/third-party-identity.js
@@ -2,4 +2,96 @@
 
 module.exports = function(Model) {
 
+  // VALIDATIONS
+
+  Model.validate('email', function(err) {
+    if (!this.getProfileEmail()) {
+      err();
+    }
+  }, { message: 'the email can\'t be blank ' });
+
+  // INSTANCE METHODS
+
+  Model.prototype.getProfileEmail = function() {
+    return this.profile && this.profile.emails && this.profile.emails.length > 0
+      ? this.profile.emails[0].value
+      : null;
+  };
+
+  Model.prototype.useExistedUser = function(user) {
+    const UserAccount = Model.app.models.UserAccount;
+
+    return new Promise((resolve, reject) => {
+      UserAccount.destroyById(this.userId, (err) => {
+        if (err) reject(err);
+        else this.updateAttribute('userId', user.id, (err) => {
+          if (err) reject(err);
+          else user.updateAttribute('isThirdParty', true)
+            .then(() => user.forceVerifyAccount())
+            .then(() => resolve())
+            .catch((err) => reject(err));
+        })
+      });
+    });
+  };
+
+  Model.prototype.setThirdPartyDataOnUser = function() {
+    const UserAccount = Model.app.models.UserAccount;
+
+    return new Promise((resolve, reject) => {
+      UserAccount.findById(this.userId, (err, user) => {
+        if (err) reject(err);
+        else {
+          let profile = this.profile;
+          if (profile) {
+            if (profile.emails && profile.emails.length > 0) {
+              let email = profile.emails[0].value;
+              if (email && email.trim() !== '') {
+                user.email = email;
+              }
+            }
+
+            if (profile.displayName) {
+              user.name = profile.displayName;
+            }
+
+            if (profile.photos && profile.photos.length > 0) {
+              user.pictureUrl = profile.photos[0].value;
+            }
+
+            user.save((err, saved) => {
+              if (err) reject(err);
+              else saved.forceVerifyAccount()
+                .then(() => resolve())
+                .catch((err) => reject(err));
+            });
+          } else {
+            resolve();
+          }
+        }
+      });
+    });
+  };
+
+  // CUSTOM METHODS
+
+  Model.onAfterCreated = function(instance) {
+    const UserAccount = Model.app.models.UserAccount;
+    return UserAccount.findOne({ where: { email: instance.getProfileEmail() }})
+      .then(user => user
+        ? instance.useExistedUser(user)
+        : instance.setThirdPartyDataOnUser());
+  };
+
+  // OPERATION HOOKS
+
+  Model.observe('after save', function(context, next) {
+    if (context.isNewInstance) {
+      Model.onAfterCreated(context.instance)
+        .then(() => next())
+        .catch((err) => next(err));
+    } else {
+      next();
+    }
+  });
 };
--- common/models/user-account.js
+++ common/models/user-account.js
@@ -18,4 +18,118 @@ module.exports = function(Model) {
   Model.disableRemoteMethodByName('prototype.__findById__identities');
   Model.disableRemoteMethodByName('prototype.__get__identities');
   Model.disableRemoteMethodByName('prototype.__updateById__identities');
+
+  // INSTANCE METHODS
+
+  Model.prototype.generateVerificationTokenAndSave = function () {
+    return new Promise((resolve, reject) => {
+      Model.generateVerificationToken(this, null, (err, token) => {
+        if (err) reject(err);
+        else {
+          this.verificationToken = token;
+          this.save((err, saved) => {
+            if (err) reject(err);
+            else resolve(saved);
+          });
+        }
+      });
+    });
+  };
+
+  Model.prototype.sendUserAccountVerificationEmail = function () {
+    return Promise.resolve();
+  };
+
+  Model.prototype.sendUserAccountResetPasswordEmail = function (accessToken) {
+    return Promise.resolve();
+  };
+
+  Model.prototype.verifyAccount = function () {
+    return this.generateVerificationTokenAndSave()
+      .then(() => {
+        return this.sendUserAccountVerificationEmail();
+      })
+      .catch(() => this.destroy());
+  };
+
+  Model.prototype.forceVerifyAccount = function () {
+    this.verificationToken = null;
+    this.emailVerified = true;
+    return this.save();
+  };
+
+  Model.prototype.handleCreatedByThirdParty = function () {
+    if (this.username && this.username.startsWith('facebook-token')) {
+      delete this.__data.username;
+      this.isThirdParty = true;
+    }
+  };
+
+  // HELPER METHODS
+
+  Model.validatePasswordLength = function (password, next) {
+    if (!password || password.length < 6) {
+      const err = new Error('The password should be less than 6 characters');
+      err.statusCode = 422;
+      err.details = {
+        context: 'UserAccount',
+        codes: {
+          password: [
+            'length.min'
+          ]
+        }
+      };
+
+      next(err);
+      return false;
+    }
+
+    return true;
+  };
+
+  // OPERATION HOOKS
+
+  Model.observe('before save', function(context, next) {
+    if (context.isNewInstance) {
+      context.instance.handleCreatedByThirdParty();
+    }
+
+    next();
+  });
+
+  Model.observe('after save', function(context, next) {
+    if (context.isNewInstance) {
+      if (context.instance.isThirdParty) next();
+      else context.instance.verifyAccount()
+        .then(() => next())
+        .catch((err) => next(err));
+    } else {
+      next();
+    }
+  });
+
+  // REMOTE HOOKS
+
+  Model.beforeRemote('setPassword', function(context, instance, next) {
+    if (Model.validatePasswordLength(context.args.newPassword, next)) {
+      next();
+    }
+  });
+
+  Model.afterRemote('resetPassword', function(context, instance, next) {
+    context.res.json({
+      title: 'Password reset requested',
+      content: 'Check your email for further instructions'
+    });
+
+    next();
+  });
+
+  // ON
+
+  Model.on('resetPasswordRequest', function(info) {
+    info.user
+      .sendUserAccountResetPasswordEmail(info.accessToken)
+      .catch((err) => console.log(err));
+  });
 };
